a64f30d253d67f4ddc14855ff10979d2b226ddef
Aman062003 <aman30033727@gmail.com>
2024-04-27 18:25:33 +0530

    Initial commit

---
+++ server/models/Users.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+})
+
+const UserModel = mongoose.model("User", UserSchema)
+
+module.exports = UserModel;
